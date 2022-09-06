e70f63616154a9a3e58f3355b42ca4279434e43e
Pierre Rineau <pierre.rineau@makina-corpus.com>
2022-09-06 15:16:35 +0200

    issue #4 - unit tests cleanup

--- tests/BasicQueryTest.php
+++ tests/BasicQueryTest.php
@@ -2,14 +2,14 @@
 
 namespace MakinaCorpus\Lucene\Tests;
 
-use DateTime;
 use MakinaCorpus\Lucene\AbstractQuery;
 use MakinaCorpus\Lucene\Query;
 use PHPUnit\Framework\TestCase;
+use DateTime;
 
 /**
- * @coversDefaultClass  \MakinaCorpus\Lucene\Query
- * @coversDefaultClass  \MakinaCorpus\Lucene\AbstractQuery
+ * @coversDefaultClass \MakinaCorpus\Lucene\Query
+ * @coversDefaultClass \MakinaCorpus\Lucene\AbstractQuery
  */
 class BasicQueryTest extends TestCase
 {
@@ -17,7 +17,9 @@ class BasicQueryTest extends TestCase
     {
         $query = new Query();
 
-        $now = new DateTime("@1467205820");
+        // Avoid runtime timezone conversion given false negative.
+        $from = new DateTime("1983-03-22", new \DateTimeZone("UTC"));
+        $now = new DateTime("@1467205820", new \DateTimeZone("UTC"));
 
         $query
             ->setOperator(Query::OP_AND)
@@ -28,47 +30,75 @@ class BasicQueryTest extends TestCase
         $query
             ->createDateRange('my_date_field')
             ->setInclusive()
-            ->setRange('1983-03-22', $now);
+            ->setRange($from, $now);
 
         $query
             ->matchTerm('some_field', 'some value', null, 0.8)
             ->matchTerm('other_field', 'some_other_value', 1.3);
 
         // @codingStandardsIgnoreStart
-        $this->assertSame(
+        self::assertSame(
             '((foo OR bar) AND my_date_field:["1983\-03\-22T00\:00\:00\+0000" TO "2016\-06\-29T13\:10\:20\+0000"] AND some_field:"some value"~0.8 AND other_field:some_other_value^1.3)',
             trim((string)$query)
         );
         // @codingStandardsIgnoreEnd
     }
 
-    /**
-     * @covers \MakinaCorpus\Lucene\AbstractQuery::escapeToken
-     */
-    public function testSpecialCharacterEscaping()
+    public function dataSpecialCharacterEscaping()
     {
-        // @codingStandardsIgnoreStart
-        $test_strings = [
-            'Use the "~" symbol to add boost to your fields!' => 'Use the \"\~\" symbol to add boost to your fields\!',
-            'Some date formats are: \"(30/12/1993), {30-12-1993} & [30\\12\\1993]\"' => 'Some date formats are\: \\\\\"\(30\/12\/1993\), \{30\-12\-1993\} \& \[30\\\12\\\1993\]\\\\\"',
-            'one + one = two' => 'one \+ one \= two',
-            'if (something || (something_else && another_something))' => 'if \(something \|\| \(something_else \&\& another_something\)\)',
-            'Here is a cat: ^*>.<*^!!' => 'Here is a cat\: \^\*\>.\<\*\^\!\!',
-            'Couldn\'t you come up with better test Strings? ... NO, sorry!' => 'Couldn\'t you come up with better test Strings\? ... NO, sorry\!',
+        return [
+            [
+                'Use the "~" symbol to add boost to your fields!',
+                'Use the \"\~\" symbol to add boost to your fields\!',
+            ],
+            [
+                'Some date formats are: \"(30/12/1993), {30-12-1993} & [30\\12\\1993]\"',
+                'Some date formats are\: \\\\\"\(30\/12\/1993\), \{30\-12\-1993\} \& \[30\\\12\\\1993\]\\\\\"',
+            ],
+            [
+                'one + one = two',
+                'one \+ one \= two',
+            ],
+            [
+                'if (something || (something_else && another_something))',
+                'if \(something \|\| \(something_else \&\& another_something\)\)',
+            ],
+            [
+                'Here is a cat: ^*>.<*^!!',
+                'Here is a cat\: \^\*\>.\<\*\^\!\!',
+            ],
+            [
+                'Couldn\'t you come up with better test Strings? ... NO, sorry!',
+                'Couldn\'t you come up with better test Strings\? ... NO, sorry\!',
+            ],
         ];
-        // @codingStandardsIgnoreEnd
+    }
 
-        // Assert strings with characters that should be escaped.
-        foreach ($test_strings as $input => $expected) {
-            $this->assertEquals("\"$expected\"", AbstractQuery::escapeToken($input));
-        }
-        // Assert single word without characters that should be escaped.
-        $this->assertEquals('Word', AbstractQuery::escapeToken('Word'));
-        // Assert phrase without characters that should be escaped.
-        $this->assertEquals('"Random search phrase."', AbstractQuery::escapeToken('Random search phrase.'));
-        /** Assert single word without characters that should be escaped, using @param $force = true. */
-        $this->assertEquals('"Word"', AbstractQuery::escapeToken('Word', TRUE));
-        // Assert string without spaces, with characters that should be escaped.
-        $this->assertEquals('"\!\*Test.php, \*.php"', AbstractQuery::escapeToken('!*Test.php, *.php'));
+    /**
+     * @dataProvider dataSpecialCharacterEscaping
+     */
+    public function testSpecialCharacterEscaping(string $raw, string $expected)
+    {
+        self::assertSame('"' . $expected . '"', AbstractQuery::escapeToken($raw));
+    }
+
+    public function testEscapeSingleWordNoQuotes()
+    {
+        self::assertSame('Word', AbstractQuery::escapeToken('Word'));
+    }
+
+    public function testEscapePhraseAddQuotes()
+    {
+        self::assertSame('"Random search phrase."', AbstractQuery::escapeToken('Random search phrase.'));
+    }
+
+    public function testEscapeSingleWordAddQuoteWithForceParam()
+    {
+        self::assertSame('"Word"', AbstractQuery::escapeToken('Word', true));
+    }
+
+    public function testEscapeWithSpecialCharsAddQuotes()
+    {
+        self::assertSame('"\!\*Test.php, \*.php"', AbstractQuery::escapeToken('!*Test.php, *.php'));
     }
 }
